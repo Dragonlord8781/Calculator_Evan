8c207de07fb05829644ca597d29f20d49e685e7b
Dragonlord8781 <evan.stone638@gmail.com>
2025-09-19 09:37:01 -0500

    Finished
    fixed problems
    added a try catch
    added comments

--- Calculator_Evan/Calculator.Designer.cs
+++ Calculator_Evan/Calculator.Designer.cs
@@ -46,15 +46,18 @@
             btnMult = new Button();
             btnSub = new Button();
             btnAdd = new Button();
+            btnDes = new Button();
             SuspendLayout();
             // 
             // lblTtitle
             // 
             lblTtitle.AutoSize = true;
-            lblTtitle.Font = new Font("Viner Hand ITC", 14.1F, FontStyle.Regular, GraphicsUnit.Point);
-            lblTtitle.Location = new Point(351, 35);
+            lblTtitle.BackColor = Color.White;
+            lblTtitle.Font = new Font("Viner Hand ITC", 20.1F, FontStyle.Regular, GraphicsUnit.Point);
+            lblTtitle.ForeColor = Color.Maroon;
+            lblTtitle.Location = new Point(286, 73);
             lblTtitle.Name = "lblTtitle";
-            lblTtitle.Size = new Size(409, 76);
+            lblTtitle.Size = new Size(583, 108);
             lblTtitle.TabIndex = 0;
             lblTtitle.Text = "disesdi digatlisisgi";
             lblTtitle.Click += lblTtitle_Click;
@@ -63,184 +66,249 @@
             // result
             // 
             result.AutoSize = true;
-            result.Font = new Font("Viner Hand ITC", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            result.Location = new Point(34, 179);
+            result.BackColor = Color.White;
+            result.Font = new Font("Viner Hand ITC", 15.9000006F, FontStyle.Regular, GraphicsUnit.Point);
+            result.ForeColor = Color.Maroon;
+            result.ImageAlign = ContentAlignment.TopRight;
+            result.Location = new Point(342, 216);
+            result.MaximumSize = new Size(1000, 1000);
+            result.MinimumSize = new Size(500, 87);
             result.Name = "result";
-            result.Size = new Size(231, 65);
+            result.Size = new Size(500, 87);
             result.TabIndex = 1;
             result.Text = "nulistanv:\r\n";
+            result.TextAlign = ContentAlignment.MiddleRight;
             result.Click += lblResult_Click;
             result.Layout += lblResult_Layout;
             // 
             // button7
             // 
+            button7.BackColor = Color.White;
             button7.Font = new Font("Viner Hand ITC", 11.1F, FontStyle.Regular, GraphicsUnit.Point);
+            button7.ForeColor = Color.Maroon;
             button7.Location = new Point(215, 342);
             button7.Name = "button7";
             button7.Size = new Size(149, 132);
             button7.TabIndex = 2;
             button7.Text = "7";
-            button7.UseVisualStyleBackColor = true;
+            button7.UseVisualStyleBackColor = false;
             button7.Click += button7_Click;
             // 
             // button8
             // 
+            button8.BackColor = Color.White;
             button8.Font = new Font("Viner Hand ITC", 11.1F, FontStyle.Regular, GraphicsUnit.Point);
+            button8.ForeColor = Color.Maroon;
             button8.Location = new Point(392, 342);
             button8.Name = "button8";
             button8.Size = new Size(149, 132);
             button8.TabIndex = 3;
             button8.Text = "8";
-            button8.UseVisualStyleBackColor = true;
+            button8.UseVisualStyleBackColor = false;
             button8.Click += button8_Click;
             // 
             // button9
             // 
+            button9.BackColor = Color.White;
             button9.Font = new Font("Viner Hand ITC", 11.1F, FontStyle.Regular, GraphicsUnit.Point);
+            button9.ForeColor = Color.Maroon;
             button9.Location = new Point(572, 342);
             button9.Name = "button9";
             button9.Size = new Size(149, 132);
             button9.TabIndex = 4;
             button9.Text = "9";
-            button9.UseVisualStyleBackColor = true;
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += button9_Click;
             // 
             // button4
             // 
+            button4.BackColor = Color.White;
             button4.Font = new Font("Viner Hand ITC", 11.1F, FontStyle.Regular, GraphicsUnit.Point);
+            button4.ForeColor = Color.Maroon;
             button4.Location = new Point(215, 516);
             button4.Name = "button4";
             button4.Size = new Size(149, 132);
             button4.TabIndex = 5;
             button4.Text = "4";
-            button4.UseVisualStyleBackColor = true;
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
             // 
             // button5
             // 
+            button5.BackColor = Color.White;
             button5.Font = new Font("Viner Hand ITC", 11.1F, FontStyle.Regular, GraphicsUnit.Point);
+            button5.ForeColor = Color.Maroon;
             button5.Location = new Point(392, 516);
             button5.Name = "button5";
             button5.Size = new Size(149, 132);
             button5.TabIndex = 6;
             button5.Text = "5";
-            button5.UseVisualStyleBackColor = true;
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
             // 
             // button6
             // 
+            button6.BackColor = Color.White;
             button6.Font = new Font("Viner Hand ITC", 11.1F, FontStyle.Regular, GraphicsUnit.Point);
+            button6.ForeColor = Color.Maroon;
             button6.Location = new Point(572, 516);
             button6.Name = "button6";
             button6.Size = new Size(149, 132);
             button6.TabIndex = 7;
             button6.Text = "6";
-            button6.UseVisualStyleBackColor = true;
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
             // 
             // button1
             // 
+            button1.BackColor = Color.White;
             button1.Font = new Font("Viner Hand ITC", 11.1F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = Color.Maroon;
             button1.Location = new Point(215, 691);
             button1.Name = "button1";
             button1.Size = new Size(149, 132);
             button1.TabIndex = 8;
             button1.Text = "1";
-            button1.UseVisualStyleBackColor = true;
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
+            button2.BackColor = Color.White;
             button2.Font = new Font("Viner Hand ITC", 11.1F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.ForeColor = Color.Maroon;
             button2.Location = new Point(392, 691);
             button2.Name = "button2";
             button2.Size = new Size(149, 132);
             button2.TabIndex = 9;
             button2.Text = "2";
-            button2.UseVisualStyleBackColor = true;
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
             // 
             // button3
             // 
+            button3.BackColor = Color.White;
             button3.Font = new Font("Viner Hand ITC", 11.1F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.ForeColor = Color.Maroon;
             button3.Location = new Point(572, 691);
             button3.Name = "button3";
             button3.Size = new Size(149, 132);
             button3.TabIndex = 10;
             button3.Text = "3";
-            button3.UseVisualStyleBackColor = true;
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
             // 
             // button0
             // 
+            button0.BackColor = Color.White;
             button0.Font = new Font("Viner Hand ITC", 11.1F, FontStyle.Regular, GraphicsUnit.Point);
+            button0.ForeColor = Color.Maroon;
             button0.Location = new Point(392, 855);
             button0.Name = "button0";
             button0.Size = new Size(149, 132);
             button0.TabIndex = 11;
             button0.Text = "0";
-            button0.UseVisualStyleBackColor = true;
+            button0.UseVisualStyleBackColor = false;
+            button0.Click += button0_Click;
             // 
             // buttonCE
             // 
+            buttonCE.BackColor = Color.White;
             buttonCE.Font = new Font("Viner Hand ITC", 11.1F, FontStyle.Regular, GraphicsUnit.Point);
+            buttonCE.ForeColor = Color.Maroon;
             buttonCE.Location = new Point(215, 855);
             buttonCE.Name = "buttonCE";
             buttonCE.Size = new Size(149, 132);
             buttonCE.TabIndex = 12;
-            buttonCE.Text = "CE";
-            buttonCE.UseVisualStyleBackColor = true;
+            buttonCE.Text = "tsu";
+            buttonCE.UseVisualStyleBackColor = false;
+            buttonCE.Click += buttonCE_Click;
             // 
             // btnE
             // 
+            btnE.BackColor = Color.White;
             btnE.Font = new Font("Viner Hand ITC", 11.1F, FontStyle.Regular, GraphicsUnit.Point);
-            btnE.Location = new Point(572, 855);
+            btnE.ForeColor = Color.Maroon;
+            btnE.Location = new Point(215, 1017);
             btnE.Name = "btnE";
-            btnE.Size = new Size(149, 132);
+            btnE.Size = new Size(506, 132);
             btnE.TabIndex = 13;
             btnE.Text = "=";
-            btnE.UseVisualStyleBackColor = true;
+            btnE.UseVisualStyleBackColor = false;
             btnE.Click += buttonEquals_Click;
             // 
             // btnDiv
             // 
+            btnDiv.BackColor = Color.White;
             btnDiv.Font = new Font("Viner Hand ITC", 11.1F, FontStyle.Regular, GraphicsUnit.Point);
+            btnDiv.ForeColor = Color.Maroon;
             btnDiv.Location = new Point(764, 342);
             btnDiv.Name = "btnDiv";
             btnDiv.Size = new Size(149, 132);
             btnDiv.TabIndex = 14;
             btnDiv.Text = "/";
-            btnDiv.UseVisualStyleBackColor = true;
+            btnDiv.UseVisualStyleBackColor = false;
+            btnDiv.Click += btnDiv_Click;
             // 
             // btnMult
             // 
+            btnMult.BackColor = Color.White;
             btnMult.Font = new Font("Viner Hand ITC", 11.1F, FontStyle.Regular, GraphicsUnit.Point);
+            btnMult.ForeColor = Color.Maroon;
             btnMult.Location = new Point(764, 516);
             btnMult.Name = "btnMult";
             btnMult.Size = new Size(149, 132);
             btnMult.TabIndex = 15;
             btnMult.Text = "X";
-            btnMult.UseVisualStyleBackColor = true;
+            btnMult.UseVisualStyleBackColor = false;
             btnMult.Click += buttonMultiply_Click;
             // 
             // btnSub
             // 
+            btnSub.BackColor = Color.White;
             btnSub.Font = new Font("Viner Hand ITC", 11.1F, FontStyle.Regular, GraphicsUnit.Point);
+            btnSub.ForeColor = Color.Maroon;
             btnSub.Location = new Point(764, 691);
             btnSub.Name = "btnSub";
             btnSub.Size = new Size(149, 132);
             btnSub.TabIndex = 16;
             btnSub.Text = "-";
-            btnSub.UseVisualStyleBackColor = true;
+            btnSub.UseVisualStyleBackColor = false;
+            btnSub.Click += btnSub_Click;
             // 
             // btnAdd
             // 
+            btnAdd.BackColor = Color.White;
             btnAdd.Font = new Font("Viner Hand ITC", 11.1F, FontStyle.Regular, GraphicsUnit.Point);
+            btnAdd.ForeColor = Color.Maroon;
             btnAdd.Location = new Point(764, 855);
             btnAdd.Name = "btnAdd";
             btnAdd.Size = new Size(149, 132);
             btnAdd.TabIndex = 17;
             btnAdd.Text = "+";
-            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.UseVisualStyleBackColor = false;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // btnDes
+            // 
+            btnDes.BackColor = Color.White;
+            btnDes.Font = new Font("Viner Hand ITC", 11.1F, FontStyle.Regular, GraphicsUnit.Point);
+            btnDes.ForeColor = Color.Maroon;
+            btnDes.Location = new Point(563, 855);
+            btnDes.Name = "btnDes";
+            btnDes.Size = new Size(149, 132);
+            btnDes.TabIndex = 18;
+            btnDes.Text = ".";
+            btnDes.UseVisualStyleBackColor = false;
+            btnDes.Click += btnDes_Click;
             // 
             // Abacus
             // 
             AutoScaleDimensions = new SizeF(19F, 48F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1080, 1127);
+            BackColor = Color.IndianRed;
+            ClientSize = new Size(1124, 1247);
+            Controls.Add(btnDes);
             Controls.Add(btnAdd);
             Controls.Add(btnSub);
             Controls.Add(btnMult);
@@ -262,7 +330,7 @@
             Font = new Font("Viner Hand ITC", 9F, FontStyle.Regular, GraphicsUnit.Point);
             Margin = new Padding(3, 4, 3, 4);
             Name = "Abacus";
-            Text = "Form1";
+            Text = "Cherokee Calculator";
             Load += Calculator_Load;
             ResumeLayout(false);
             PerformLayout();
@@ -288,5 +356,6 @@
         private Button btnMult;
         private Button btnSub;
         private Button btnAdd;
+        private Button btnDes;
     }
 }
--- Calculator_Evan/Calculator.cs
+++ Calculator_Evan/Calculator.cs
@@ -1,93 +1,126 @@
+using System.Data;
+using System;
+
 namespace Calculator_Evan
 {
     public partial class Abacus : Form
     {
-        string resultString;
+        string resultString; 
 
         public Abacus()
         {
             InitializeComponent();
 
-            result.Text = " ";
+            //makes resultString=result, make the result box empty on start
+            result.Text = "";
             resultString = result.Text;
         }
 
         private void Calculator_Load(object sender, EventArgs e)
         {
-
+            //null
         }
 
         private void buttonEquals_Click(object sender, EventArgs e)
         {
+            /*
+             try to calculate answer, if error occurs, catch by replacing result.Text with error, if not calculate 
+            the answer an replace result.Text with it
+            */
+            try
+            {
+                object resultCalc = new DataTable().Compute(resultString, null);
 
+                double calculatedValue = Convert.ToDouble(resultCalc);
+
+                resultString = calculatedValue.ToString();
+                result.Text = resultString;
+            }
+            catch
+            {
+                resultString = "galidasdv"; //galidasdv means error
+                result.Text = resultString;
+            }
         }
 
+        //add * to resultString, make result.Text = result string
         private void buttonMultiply_Click(object sender, EventArgs e)
         {
             resultString += "*";
             result.Text = resultString;
         }
 
+        //add 7 to resultString, make result.Text = result string
         private void button7_Click(object sender, EventArgs e)
         {
             resultString += "7";
             result.Text = resultString;
         }
 
+        //add 8 to resultString, make result.Text = result string
         private void button8_Click(object sender, EventArgs e)
         {
             resultString += "8";
             result.Text = resultString;
         }
 
+        //add 9 to resultString, make result.Text = result string
         private void button9_Click(object sender, EventArgs e)
         {
             resultString += "9";
             result.Text = resultString;
         }
 
+        //add / to resultString, make result.Text = result string
         private void btnDiv_Click(object sender, EventArgs e)
         {
             resultString += "/";
             result.Text = resultString;
         }
 
+        //add 4 to resultString, make result.Text = result string
         private void button4_Click(object sender, EventArgs e)
         {
             resultString += "4";
             result.Text = resultString;
         }
 
+        //add 5 to resultString, make result.Text = result string
         private void button5_Click(object sender, EventArgs e)
         {
             resultString += "5";
             result.Text = resultString;
         }
 
+        //add 6 to resultString, make result.Text = result string
         private void button6_Click(object sender, EventArgs e)
         {
             resultString += "6";
             result.Text = resultString;
         }
 
+        //add 1 to resultString, make result.Text = result string
         private void button1_Click(object sender, EventArgs e)
         {
             resultString += "1";
             result.Text = resultString;
         }
 
+        //add 2 to resultString, make result.Text = result string
         private void button2_Click(object sender, EventArgs e)
         {
             resultString += "2";
             result.Text = resultString;
         }
 
+        //add 3 to resultString, make result.Text = result string
         private void button3_Click(object sender, EventArgs e)
         {
             resultString += "3";
             result.Text = resultString;
         }
 
+        //add - to resultString, make result.Text = result string
         private void btnSub_Click(object sender, EventArgs e)
         {
             resultString += "-";
@@ -96,40 +129,46 @@ namespace Calculator_Evan
 
         private void lblResult_Layout(object sender, EventArgs e)
         {
-
+            //null
         }
 
         private void lblTtitle_Click(object sender, EventArgs e)
         {
-
+            //null
         }
 
         private void label1_Layout(object sender, EventArgs e)
         {
-
+            //null
         }
 
         private void lblResult_Click(object sender, EventArgs e)
         {
-
+            //null
         }
+
+        //clears result
         private void buttonCE_Click(object sender, EventArgs e)
         {
-
+            resultString = "";
+            result.Text = resultString;
         }
 
+        //add 0 to resultString, make result.Text = result string
         private void button0_Click(object sender, EventArgs e)
         {
             resultString += "0";
             result.Text = resultString;
         }
 
+        //add + to resultString, make result.Text = result string
         private void btnAdd_Click(object sender, EventArgs e)
         {
             resultString += "+";
             result.Text = resultString;
         }
 
+        //add . to resultString, make result.Text = result string
         private void btnDes_Click(object sender, EventArgs e)
         {
             resultString += ".";
